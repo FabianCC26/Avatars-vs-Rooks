d095cd4a451219a597a8d1c248d93f26d0eaeead
deinol18 <98658922+deinol18@users.noreply.github.com>
2025-10-20 12:20:15 -0600

    Se creó el código para el login de los usuarios

---
+++ tests/test_login.py
@@ -0,0 +1,18 @@
+
+from src.auth.login_user import login_por_username
+
+def probar_login():
+    print("=== PRUEBA DE LOGIN POR USERNAME ===")
+    username = input("Usuario: ")
+    password = input("Contraseña: ")
+
+    resultado = login_por_username(username, password)
+
+    if resultado:
+        print(f"\n👤 Bienvenido {resultado.get('nombre', 'Usuario')}")
+        print(f"🔹 Rol: {resultado.get('rol', 'Sin rol asignado')}")
+    else:
+        print("\n❌ Error en autenticación.")
+
+if __name__ == "__main__":
+    probar_login()
