11b805e883e2a1b03948bed382448d3f88f5b80f
Jimena <jmaldoqu@gmail.com>
2025-10-21 21:13:21 -0600

    Arreglos en la UI

--- tests/test_main_window.py
+++ tests/test_main_window.py
@@ -4,7 +4,8 @@ from src.config import settings
 
 
 if __name__ == "__main__":
-    window = MainWindow()
+    image_path = "src/assets/images/default_photo.png"
+    window = MainWindow("Admin.", "Admin123", image_path)
     window.run()
 
     
